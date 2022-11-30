ceb2c954b9616d5caf7138583c789cd65019437a
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-11-30 17:11:32 +0530

    removed hard coded html code

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,18 +1,20 @@
-function ExpenseItem() {
-  return (
-    /*<div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>price</div>
-      </div>
-    </div>*/
-    <div>
-    <h4>Food Rs 10</h4>
-    <h4>Movies Rs 200</h4>
-    <h4>Petrol 100</h4>
-    </div>
+import "./ExpenseItem.css";
 
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 4, 25);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 2500;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+    </div>
   );
 }
+
 export default ExpenseItem;
+/**Because the code we are writing is not real html code. class is used to assign class name to JSX. */
