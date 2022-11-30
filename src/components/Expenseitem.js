a3904d720e40fbc6d5a694f19e8cf9874294acf7
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-11-30 12:25:51 +0530

    ExpenseItem component added

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+  return (
+    /*<div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>price</div>
+      </div>
+    </div>*/
+    <div>
+    <h4>Food Rs 10</h4>
+    <h4>Movies Rs 200</h4>
+    <h4>Petrol 100</h4>
+    </div>
+
+  );
+}
+export default ExpenseItem;
