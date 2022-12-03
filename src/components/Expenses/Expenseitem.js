88f322b8a7608ba7b6fb88dc3c076e9193a3eddd
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-12-03 13:11:58 +0530

    state functionality added

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -1,25 +1,29 @@
-import react from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseAmountTitle from "./ExpenseAmount_title";
 import Card from "../UI/card";
+import { useState } from "react";
 
-const ExpenseItem=(props) =>{
-  const clickHandler=()=>{console.log('clicked')}
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <h4>{props.location}</h4>
         <ExpenseAmountTitle amount={props.amount} />
       </div>
       <button onClick={clickHandler}>Change Title</button>
       <button>Delete Expense</button>
-
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
