18d613079faa79b0aeb7d829286f862cba97eb65
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-12-06 12:28:29 +0530

    looping through array and key added

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import "./components/UI/card.css";
-import { useEffect, useState } from "react";
-import ExpensesFilter from "./components/Expenses/ExpenseFilter";
+import { useState } from "react";
+const dummyExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-  const [array, setArray] = useState(expenses);
+  const [expenses, setArray] = useState(dummyExpenses);
 
   const addExpenseHandler = (expens) => {
-    setArray([...array, expens]);
+    //setArray([...array, expens]);
+    setArray((prevExpense) => {
+      return [expens, ...prevExpense];
+    });
   };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <ExpensesFilter></ExpensesFilter>
-      <Expenses expenses={array} />
+      <Expenses expenses={expenses} />
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,8 +6,16 @@ import ExpensesFilter from "./ExpenseFilter";
 import React, { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      ></ExpensesFilter>
       {props.expenses.map((ele, index) => {
         return (
           <ExpenseItem
@@ -15,7 +23,6 @@ const Expenses = (props) => {
             amount={ele.amount}
             date={ele.date}
             key={ele.id}
-            id={ele.id}
           ></ExpenseItem>
         );
       })}
