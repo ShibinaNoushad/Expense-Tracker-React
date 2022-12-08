a59b2a1bf74e4b0382d7ad8a901a30f82227e468
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-12-08 13:58:01 +0530

    Expense Chart added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/card";
 import ExpensesFilter from "./ExpenseFilter";
 import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -12,18 +13,18 @@ const Expenses = (props) => {
   const filteredExpenses = props.expenses.filter((ele) => {
     return ele.date.getFullYear().toString() === filteredYear;
   });
-  
 
   return (
-    <div>
+    <li>
       <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpensesList items={filteredExpenses}/>
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses} />
       </Card>
-    </div>
+    </li>
   );
 };
 
